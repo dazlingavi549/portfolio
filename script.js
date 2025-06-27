4678be56d7a3bb04d66cb7377313ea9c5658ab51
dazlingavi549 <65861656+dazlingavi549@users.noreply.github.com>
2025-06-27 19:20:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for in-page links
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(a.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
